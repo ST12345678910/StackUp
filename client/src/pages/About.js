d0f2c0288025e1e8e71ec1415ae5435c864ac7f2
Hye Won Park <hyewonpark26@gmail.com>
2022-04-28 02:20:58 -0700

    adds four cards

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -34,7 +34,76 @@ const About = () => {
                 </div>
               </div>
 
-           
+            {/* card two */}
+              <div className="card col-md-6">
+              <h4 className="card-header bg-primary text-light p-2 m-0">Shaun Tehranchi
+                <br />
+                <span style={{ fontSize: '1rem' }}>
+                  Software Developer
+                </span>
+              </h4>
+              
+              <div className="card-body bg-light p-2">
+                <img src="https://brighterwriting.com/wp-content/uploads/icon-user-default.png" class="card-img-top" alt="..."></img>
+    
+              </div>
+              <div className="btn btn-primary btn-block btn-squared">
+                <p>Github: https://github.com/ST12345678910</p>
+                <p>LinkedIn: </p>
+                <p>Contact: </p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis delectus quibusdam nostrum laboriosam quaerat alias nihil! Quaerat labore illum, magni reiciendis placeat aliquam minima quam itaque ratione incidunt provident corporis!: </p>
+               
+              </div>
+              </div>
+            </div>
+            
+            <br></br>
+            
+            <div class="row">
+              {/* card three */}
+              <div className="card col-md-6">
+                <h4 className="card-header bg-primary text-light p-2 m-0">Shaun Tehranchi
+                  <br />
+                  <span style={{ fontSize: '1rem' }}>
+                    Software Developer
+                  </span>
+                </h4>
+
+                <div className="card-body bg-light p-2">
+                  <img src="https://brighterwriting.com/wp-content/uploads/icon-user-default.png" class="card-img-top" alt="..."></img>
+
+                </div>
+                <div className="btn btn-primary btn-block btn-squared">
+                  <p>Github: https://github.com/ST12345678910</p>
+                  <p>LinkedIn: </p>
+                  <p>Contact: </p>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis delectus quibusdam nostrum laboriosam quaerat alias nihil! Quaerat labore illum, magni reiciendis placeat aliquam minima quam itaque ratione incidunt provident corporis!: </p>
+
+                </div>
+              </div>
+
+              {/* card four */}
+              <div className="card col-md-6">
+                <h4 className="card-header bg-primary text-light p-2 m-0">Shaun Tehranchi
+                  <br />
+                  <span style={{ fontSize: '1rem' }}>
+                    Software Developer
+                  </span>
+                </h4>
+
+                <div className="card-body bg-light p-2">
+                  <img src="https://brighterwriting.com/wp-content/uploads/icon-user-default.png" class="card-img-top" alt="..."></img>
+
+                </div>
+                <div className="btn btn-primary btn-block btn-squared">
+                  <p>Github: https://github.com/ST12345678910</p>
+                  <p>LinkedIn: </p>
+                  <p>Contact: </p>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis delectus quibusdam nostrum laboriosam quaerat alias nihil! Quaerat labore illum, magni reiciendis placeat aliquam minima quam itaque ratione incidunt provident corporis!: </p>
+
+                </div>
+              </div>
+            </div>
 
           </div>
 
